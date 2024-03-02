85b3b5c44bb4e9cabc7bf85829b86cbd453d5618
Marica Romano <marica.romano@studio.unibo.it>
2024-03-02 13:55:55 +0100

    Refactoring class Test

--- pps-lab01-intellij-basic-example/test/BankAccountWithATMTest.java
+++ pps-lab01-intellij-basic-example/test/BankAccountWithATMTest.java
@@ -11,14 +11,13 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class BankAccountWithATMTest {
 
     private static final double TEST_AMOUNT_OF_MONEY = 100.0;
-    private static final int ID_TEST = 1;
 
     private AccountHolder accountHolder;
     private BankAccount bankAccount;
 
     @BeforeEach
     void beforeEach() {
-        accountHolder = new AccountHolder("Mario", "Rossi", ID_TEST);
+        accountHolder = new AccountHolder("Mario", "Rossi", 1);
         bankAccount = new BankAccountWithATM(accountHolder, 0.0);
     }
 
@@ -34,14 +33,14 @@ public class BankAccountWithATMTest {
 
     @Test
     void depositWithATMTest() {
-        this.bankAccount.deposit(ID_TEST, TEST_AMOUNT_OF_MONEY);
+        this.bankAccount.deposit(this.accountHolder.getId(), TEST_AMOUNT_OF_MONEY);
         assertEquals(99.0, this.bankAccount.getBalance());
     }
 
     @Test
     void withdrawWithATMTest() {
-        this.bankAccount.deposit(ID_TEST, 2 * TEST_AMOUNT_OF_MONEY);
-        this.bankAccount.withdraw(ID_TEST, TEST_AMOUNT_OF_MONEY);
+        this.bankAccount.deposit(this.accountHolder.getId(), 2 * TEST_AMOUNT_OF_MONEY);
+        this.bankAccount.withdraw(this.accountHolder.getId(), TEST_AMOUNT_OF_MONEY);
         double expectedValue = 2 * TEST_AMOUNT_OF_MONEY - TEST_AMOUNT_OF_MONEY - 2;
         assertEquals(expectedValue, this.bankAccount.getBalance());
     }
@@ -49,5 +48,11 @@ public class BankAccountWithATMTest {
     @Test
     void depositWrongIdTest() {
         assertThrows(IllegalArgumentException.class, () -> this.bankAccount.deposit(2, TEST_AMOUNT_OF_MONEY));
+    }
+
+    @Test
+    void withdrawWrongIdTest() {
+        this.bankAccount.deposit(this.accountHolder.getId(), 2*TEST_AMOUNT_OF_MONEY);
+        assertThrows(IllegalArgumentException.class, () -> this.bankAccount.withdraw(2, TEST_AMOUNT_OF_MONEY));
     }
 }
