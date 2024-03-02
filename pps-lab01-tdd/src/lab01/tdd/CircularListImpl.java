8b8ac081e0ed5b4dc8a7599a5ab4e27669dbbb95
Marica Romano <marica.romano@studio.unibo.it>
2024-03-02 15:36:28 +0100

    Create class CircularListImpl with size() and isEmpty()

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,36 @@
+package lab01.tdd;
+
+import lab01.tdd.CircularList;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,15 +1,37 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Test public void testTodo(){
+    @BeforeEach
+    void createCircularList() {
+        this.circularList = new CircularListImpl();
+    }
+
+    @Test
+    void circularListSizeTest() {
+        assertEquals(0, this.circularList.size());
+    }
+
+    @Test
+    void isListEmptyTest() {
+        assertTrue(this.circularList.isEmpty());
+    }
+
+    @Disabled
+    public void testTodo(){
         Assertions.fail();
     }
 
