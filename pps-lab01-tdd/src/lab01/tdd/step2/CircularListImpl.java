49e9c208a989e9ed10fe220723356b4485c426da
Marica Romano <marica.romano@studio.unibo.it>
2024-03-03 16:53:54 +0100

    Create new CircularList interface, class and test class

---
+++ pps-lab01-tdd/src/lab01/tdd/step2/CircularListImpl.java
@@ -0,0 +1,30 @@
+package lab01.tdd.step2;
+
+import java.util.Iterator;
+
+public class CircularListImpl implements CircularList {
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<Integer> forwardIterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Integer> backwardIterator() {
+        return null;
+    }
+}
---
+++ pps-lab01-tdd/test/CircularListIteratorTest.java
@@ -0,0 +1,21 @@
+import lab01.tdd.step2.CircularList;
+import lab01.tdd.step2.CircularListImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CircularListIteratorTest {
+
+    private CircularList circularList;
+
+    @BeforeEach
+    void createCircularList() {
+        this.circularList = new CircularListImpl();
+    }
+
+    @Test
+    void createCorrectlyCircularListTest() {
+        assertEquals(0, this.circularList.size());
+    }
+}
