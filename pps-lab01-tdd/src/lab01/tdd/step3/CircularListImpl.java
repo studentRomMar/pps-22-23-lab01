892184eacda4fbccb5d59381eac61731a735bc59
Marica Romano <marica.romano@studio.unibo.it>
2024-03-05 12:58:37 +0100

    Create new package for step 3

---
+++ pps-lab01-tdd/src/lab01/tdd/step3/CircularListImpl.java
@@ -0,0 +1,25 @@
+package lab01.tdd.step3;
+
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Optional<Integer> filteredNext() {
+        return Optional.empty();
+    }
+}
---
+++ pps-lab01-tdd/test/CircularListFilteredNextTest.java
@@ -0,0 +1,4 @@
+import lab01.tdd.step3.*;
+
+public class CircularListFilteredNextTest {
+}
